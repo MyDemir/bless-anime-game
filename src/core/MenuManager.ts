3b969d978153c85691ddb3b6586585f0e34246d1
MyD <m.demir-21@outlook.com>
2025-05-16 23:49:40 +0300

    Update MenuManager.ts

--- src/core/MenuManager.ts
+++ src/core/MenuManager.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from './utils/EventEmitter';
+import { EventEmitter } from '../utils/EventEmitter';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
