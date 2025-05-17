c4b58274c378fc1fe9e13d56c8c8702c25df5f92
MyD <m.demir-21@outlook.com>
2025-05-18 01:44:40 +0300

    Update Game.ts

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,9 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { ModelsLoader } from '/utils/loadModels';
-import { EventEmitter } from '/utils/EventEmitter';
 import { MenuManager } from './MenuManager';
-
+import { ModelsLoader } from './../utils/loadModels';
+import { EventEmitter } from './../utils/EventEmitter';
 export class Game {
     private scene: THREE.Scene;
     private camera: THREE.PerspectiveCamera;
