2e84b17451c64b1ca5dc217d1f743d9cae02452a
MyD <m.demir-21@outlook.com>
2025-05-17 03:00:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-resolve: {
-alias: {
-'three': 'three',
-'three/examples/jsm': 'three/examples/jsm'
-}
-}
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'three': 'three',
+      'three/examples/jsm': 'three/examples/jsm'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 });
