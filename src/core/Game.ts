59628fd2f0461cb355726a19e17624f692dbbf18
MyD <m.demir-21@outlook.com>
2025-05-18 01:42:01 +0300

    Update Game.ts

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { ModelsLoader } from './../../utils/loadModels';
-import { EventEmitter } from './../../utils/EventEmitter';
+import { ModelsLoader } from './utils/loadModels';
+import { EventEmitter } from './utils/EventEmitter';
 import { MenuManager } from './MenuManager';
 
 export class Game {
