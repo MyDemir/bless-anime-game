3a69557550be4d1573dee7496455c9eefed887ec
MyD <m.demir-21@outlook.com>
2025-05-19 17:32:30 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,5 @@
 import WebServer from '@blockless/sdk-ts/dist/lib/web';
-import { initGame } from './src/core/main'; // src/core/main.ts'den oyun başlatıcısını içe aktar
 
 const server = new WebServer();
-
-// Statik dosyaları sun
 server.statics('public', '/');
-
-// Oyunu başlat
-initGame(); // src/core/main.ts'deki oyun başlatma fonksiyonunu çağır
-
-// Sunucuyu başlat
 server.start();
