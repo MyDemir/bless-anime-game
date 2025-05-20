cf79f73ced9082eba1f4cc2b8a431f6cd368cc97
MyD <m.demir-21@outlook.com>
2025-05-20 19:37:58 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,5 @@
 import WebServer from '@blockless/sdk-ts/dist/lib/web';
-
-console.log("Blockless WebServer başlatılıyor");
-
-// Web sunucusunu başlat
 const server = new WebServer();
+
 server.statics('public', '/');
 server.start();
-
-// WASM için giriş noktası
-export function main() {
-  console.log("WASM giriş noktası çalıştı");
-}
