20700d271fc547edba44d17207d67bf14a54fa25
MyD <m.demir-21@outlook.com>
2025-05-30 23:28:19 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,18 +1,5 @@
-import * as tf from '@tensorflow/tfjs';
-import { setWasmPaths } from '@tensorflow/tfjs-backend-wasm';
-import { NotificationManager } from './src/core/NotificationManager';
+import WebServer from '@blockless/sdk-ts/dist/lib/web';
+const server = new WebServer();
 
-async function initializeTfBackend(): Promise<void> {
-  try {
-    setWasmPaths('/tfjs-backend-wasm.wasm');
-    await tf.setBackend('wasm');
-    console.log('TensorFlow.js WASM backend başlatıldı');
-  } catch (error) {
-    console.error('WASM backend başlatılamadı:', error);
-    NotificationManager.getInstance().show('AI backend başlatılamadı, CPU kullanılıyor!', 'warning');
-    await tf.setBackend('cpu');
-    console.log('CPU backend aktif');
-  }
-}
-
-export { initializeTfBackend };
+server.statics('public', '/');
+server.start();
