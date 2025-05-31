32e29feb047128be64a6f72963a5e3a4ae2a28ad
MyD <m.demir-21@outlook.com>
2025-05-31 11:53:55 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
 import WebServer from '@blockless/sdk-ts/dist/lib/web';
 const server = new WebServer();
 
-// MIME türünü açıkça tanımla
-server.statics('public', '/', {
-  mimeTypes: {
-    '.wasm': 'application/wasm'
-  }
-});
+server.statics('public', '/');
 server.start();
